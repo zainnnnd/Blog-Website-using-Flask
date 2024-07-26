0475022cf00473ec6269bd60bed8a1b2418027ef
zainnnnd <abdulrahmanhaq22@gmail.com>
2024-07-26 20:44:59 +0530

    This is First Version

---
+++ app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5e376c10556f1919e77e774780f26b8a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes
+
+
+
+
+
+'''from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '5e376c10556f1919e77e774780f26b8a'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    
+    # Import and register your routes
+    from app import routes
+    app.register_blueprint(routes.main)  # Assuming you have a main blueprint
+    
+    return app
+'''
